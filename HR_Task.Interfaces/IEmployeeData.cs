e53132e0967eaccbbe25c8e6bdfc7591c353c350
Youssef <Youssef@YOUSSEF-PC>
2022-09-09 14:44:56 +0200

    Add interfaces lib

---
+++ HR_Task.Interfaces/IEmployeeData.cs
@@ -0,0 +1,15 @@
+﻿namespace HR_Task.Interfaces
+{
+    public interface IEmployeeData
+    {
+        public Task<ICollection<Employee>> GetEmployees();
+
+        public Task<Employee> GetEmployee(int id);
+
+        public Task<Employee> AddEmployee(Employee employee);
+
+        public Task<Employee> UpdateEmployees(Employee updatedEmployee);
+
+        public Task<bool> DeleteEmployee(int id);
+    }
+}
